d46f210b06888af25922d7fee79548bc2b5aa74d
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-07-13 21:47:02 +0530

    added new script in this file-2

--- 02-varialbles.sh
+++ 02-varialbles.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh: Hello Sureshs, Good  Evening"
-echo "Suresh: Hi Suresh, Veru Good Evennig"
-echo "Ramesh: How are you Suresh?"
-echo "Suresh: I am good,how are you?"
+PERSION1=Ramesh
+PERSION2=Suresh
+
+echo "$PERSION1: Hello $PERSION2 Good  Evening"
+echo "$PERSION2: Hi $PERSION1, Veru Good Evennig"
+echo "$PERSION1: How are you $PERSION2?"
+echo "$PERSION2: I am good,how are you?"
