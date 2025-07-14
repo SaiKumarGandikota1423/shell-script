dd891555e6ee887ba709bcf26718cc73eeb66f76
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-07-14 10:19:49 +0530

    we have added another file for date and time

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSION1=$1
+PERSION2=$2
+
+echo "$PERSION1: Hello $PERSION2 Good  Evening"
+echo "$PERSION2: Hi $PERSION1, Veru Good Evennig"
+echo "$PERSION1: How are you $PERSION2?"
+echo "$PERSION2: I am good $PERSION1,how are you?"
