5c8ffaf4d64fbc85a06fe0108c0be38aabbe564c
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-07-14 10:57:14 +0530

    we have added another file for date and time

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,4 +4,10 @@ echo "Please enter your username"
 
 read -s USERNAME #the value entered above will be automatically attached to USERNAME variable
 
-echo "Username is: $USERNAME"
+echo "Username is: $USERNAME" # I am printing just for validation, you shoud not print username and password
+
+echo "Please enter your password"
+
+read -s PASSWORD #the value entered above will be automatically attached to PASSWORD variable
+
+echo "Username is: $PASSWORD" # I am printing just for validation, you shoud not print username and password
