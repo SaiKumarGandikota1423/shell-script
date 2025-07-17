e91ecdaf0ad4360de158de1fc58ef406ff19744f
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-07-17 10:11:16 +0530

    we have added the condition script file

---
+++ 11-logs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ID=$(id -u)
+echo "script name: $0" 
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2 .... FAILED"
+    else
+        echo "$2 .... SUCCESS"
+    fi 
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo "ERROR:: Please run this script with root access"
+    exit 1 #you can give other than 0
+else 
+    echo "You are root user"
+fi #fi means reverse of if, it idicates the condition end
+
+yum install mysql -y
+
+VALIDATE $? "Installing MySQL"
+
+yum install git -y
+
+VALIDATE $? "Installing Git"
