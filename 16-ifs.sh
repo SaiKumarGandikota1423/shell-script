ac1db3246a36f43e2fc737f9d70c23d180d664b5
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-09-13 22:01:20 +0530

    adding new  files

---
+++ 16-ifs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+file=/etc/passwd
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $file] # ! denotes opposite
+then
+    echo -e "$R Source directory: $file does not exit. $N"
+fi
+
+while IFS=":" read -r username paasowrd user_id group_if user_fullname home_dir shell_path
+do
+    echo "username: $username"
+    echo "user_ID: $user_id"
+    echo "user Fullname: $user_fullname"
+done <<< $file
