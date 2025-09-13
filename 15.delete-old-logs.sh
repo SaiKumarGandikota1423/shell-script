2e681efebfb1cc40f18e50cac84344e0358cc199
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-09-13 21:24:08 +0530

    daws-76s

--- 15.delete-old-logs.sh
+++ 15.delete-old-logs.sh
@@ -5,16 +5,16 @@ SOURCE_DIR="/tmp/shellscript-logs"
     R="\e[31m"
     G="\e[32m"
     Y="\e[33m"
-    N="\e[0"
+    N="\e[0m"
 
 if [ ! -d $SOURCE_DIR ] # ! denotes opposite
 then
     echo -e "$R Source directory: $SOURCE_DIR doesnot exist. $N"
 fi
  
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+FILES_TO_DELETE=$(find "$SOURCE_DIR" -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line
 do
     echo "Deleting file: $line"
-done <<< $FILES_TO_DELTE
+done <<< "$FILES_TO_DELETE"
