d39df9ebf316620cce0e6c2b85289ac759aad4b9
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-09-13 21:37:54 +0530

    adding all 3 files

--- 15.delete-old-logs.sh
+++ 15.delete-old-logs.sh
@@ -7,14 +7,14 @@ SOURCE_DIR="/tmp/shellscripting-logs/"
     Y="\e[33m"
     N="\e[0m"
 
-if [ ! -d "$SOURCE_DIR" ] # ! denotes opposite
+if [ ! -d $SOURCE_DIR ] # ! denotes opposite
 then
     echo -e "$R Source directory: $SOURCE_DIR doesnot exist. $N"
 fi
  
-FILES_TO_DELETE=$(find "$SOURCE_DIR" -type f -mtime +14 -name "*.log")
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line
 do
     echo "Deleting file: $line"
-done <<< "$FILES_TO_DELETE"
+done <<< $FILES_TO_DELETE
