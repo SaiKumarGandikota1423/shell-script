c777893e8e4db9b43532f51f7455164b77e059ae
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-09-13 22:12:22 +0530

    adding new  files

--- 16-ifs.sh
+++ 16-ifs.sh
@@ -6,14 +6,14 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ ! -f $file ] # denotes opposite
-then
-    echo -e "$R Source directory: $file does not exit. $N"
+if [ ! -f "$file" ]; then
+    echo -e "$R Source file: $file does not exist. $N"
+    exit 1
 fi
 
-while IFS=":" read -r username password user_id group_if user_fullname home_dir shell_path
+while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path
 do
     echo "username: $username"
     echo "user_ID: $user_id"
     echo "user Fullname: $user_fullname"
-done <<< $file
+done < "$file"
