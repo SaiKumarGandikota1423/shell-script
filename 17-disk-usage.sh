dfef33f2d150ccdc8dd4a23dee90af1df8b86cd4
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-09-14 20:32:11 +0530

    adding new  files

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -9,10 +9,9 @@ do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $DISK_THRESHOLD ]
-    then 
+    then
         meassage+="High Disk Usage on $partition: $usage"
     fi
-
 done <<< $DISK_USAGE
 
 echo "Message:$message"
